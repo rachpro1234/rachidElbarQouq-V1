19860e8ab27ed18430fb4c25cf7fbe751ca60779
Rachid El Barqouqy <108005388+rachpro1234@users.noreply.github.com>
2024-09-17 16:33:14 +0100

    testing language switch

--- assets/js/main.js
+++ assets/js/main.js
@@ -190,3 +190,43 @@ themeButton.addEventListener("click" , () => {
 });
 
 
+
+
+   // Create a function to change
+        // the hash value of the page
+        function changeLanguage(lang) {
+            location.hash = lang;
+            location.reload();
+        }
+
+        // Define the language reload anchors
+        let language = {
+            eng: {
+                welcome: "Welcome to the GeeksforGeeks " +
+                    "Portal! You can choose any language " +
+                    "using the buttons above!"
+            },
+            es: {
+                welcome: "¡Bienvenido al portal GeeksforGeeks! " +
+                    "¡Puedes elegir cualquier idioma usando " +
+                    "los botones de arriba!"
+            },
+            de: {
+                welcome: "GeeksforGeeks ist eine sehr schone platform"
+            }
+        };
+
+        // Check if a hash value exists in the URL
+        if (window.location.hash) {
+
+            // Set the content of the webpage
+            // depending on the hash value
+            if (window.location.hash == "#es") {
+                siteContent.textContent =
+                    language.es.welcome;
+            }
+            else if (window.location.hash == "#de") {
+                siteContent.textContent =
+                    language.hin.welcome;
+            }
+        }
