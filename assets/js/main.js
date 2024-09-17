7274ff702742c5127d6c4e09a73ffbaac62e9ed0
Rachid El Barqouqy <108005388+rachpro1234@users.noreply.github.com>
2024-09-17 16:50:52 +0100

    bug fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,232 +1,232 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById("nav-menu"),
-      navToggle = document.getElementById("nav-toggle"),
-      navClose =  document.getElementById("nav-close");
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu');
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll(".nav__link")
+const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
-    const navMenu = document.getElementById("nav-menu")
-    // when we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove("show-menu")
+  const navMenu = document.getElementById("nav-menu");
+  // when we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*==================== ACCORDION SKILLS ====================*/
 
-const skillsContent = document.getElementsByClassName('skills__content'),
-      skillsHeader = document.querySelectorAll('.skills__header');
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
 
- function toggleSkills () {
-    let itemClass = this.parentNode.className;
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
 
-    for(let i = 0; i < skillsContent.length; i++) {
-        skillsContent[i].className = 'skills__content skills__close';
-    }
-    if(itemClass === 'skills__content skills__close') {
-        this.parentNode.className = 'skills__content skills__open';
-    }
- }     
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
 
- skillsHeader.forEach((el) => {
-    el.addEventListener("click", toggleSkills);
- })
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
 /*==================== QUALIFICATION TABS ====================*/
 
-const tabs = document.querySelectorAll('[data-target]'), 
-tabContents = document.querySelectorAll('[data-content]')
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
 
 tabs.forEach((tab) => {
-   tab.addEventListener('click', () => {
-     const target = document.querySelector(tab.dataset.target)
-     tabContents.forEach((tabContent) => {
-        tabContent.classList.remove("qualification__active")
-     });
-     target.classList.add("qualification__active");
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+    target.classList.add("qualification__active");
 
-     tabs.forEach((tab) => {
-        tab.classList.remove("qualification__active");
-     })
-     tab.classList.add("qualification__active");
-   })
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
 });
 
 /*==================== SERVICES MODAL ====================*/
- const modalViews = document.querySelectorAll('.services__modal'), 
-       modalBtns = document.querySelectorAll('.services__button'),
-       modalCloses = document.querySelectorAll('.services__modal-close');
-
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
 
 let modal = function (modalClick) {
-    modalViews[modalClick].classList.add('active-modal')
-}
+  modalViews[modalClick].classList.add("active-modal");
+};
 
 modalBtns.forEach((modalBtn, i) => {
-    modalBtn.addEventListener("click", () => {
-        modal(i)
-    })
-})
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
 
 modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener("click", () => {
-        modalViews.forEach((modalView) => {
-            modalView.classList.remove("active-modal");
-        });
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
     });
+  });
 });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-let swiper = new Swiper('.portfolio__container', {
-    cssMode: true,
-    loop: true,
+let swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
 
-    navigation: {
-        nextEl:".swiper-button-next",
-        prevEl:".swiper-button-prev",
-    },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
-    keyboard: true
-})
-
-
+  keyboard: true,
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
-  
- const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll("section[id]");
 
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-  function scrollActive () {
-    const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
-    sections.forEach((current) => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id');
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + "]")
-            .classList.add('active-link');
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + "]")
-            .classList.remove('active-link');
-        }
-    });
-  }
-  window.addEventListener('scroll', scrollActive);
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
-    const nav = document.getElementById('header');
+  const nav = document.getElementById("header");
 
-    if(this.scrollY >= 80) nav.classList.add('scroll-header');
-    else nav.classList.remove('scroll-header');
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
 
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener("scroll", scrollHeader);
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
-    const scrollUp = document.getElementById('scroll-up');
+  const scrollUp = document.getElementById("scroll-up");
 
-    if(this.scrollY >= 500) scrollUp.classList.add('show-scroll')
-    else scrollUp.classList.remove('show-scroll')
+  if (this.scrollY >= 500) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-    window.addEventListener('scroll', scrollUp);
+window.addEventListener("scroll", scrollUp);
 
+/*==================== DARK LIGHT THEME ====================*/
 
-/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
 
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'uil-sun';
-
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 const getCurrentTheme = () => {
-    document.body.classList.contains(darkTheme) ? 'dark' : 'light' };
-
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+};
 
 const getCurrentIcon = () => {
-   themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun' };
-
-
-if(selectedTheme) {
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 };
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
 
 // on/off the theme manually with the button
 
-themeButton.addEventListener("click" , () => {
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme);
-   // save it in the local-storage
-    localStorage.setItem('selected-theme', getCurrentTheme());
-    localStorage.setItem('selected-icon', getCurrentIcon());
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // save it in the local-storage
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
+// Create a function to change
+// the hash value of the page
+function changeLanguage(lang) {
+  location.hash = lang;
+  location.reload();
+}
 
+// Define the language reload anchors
+let language = {
+  eng: {
+    welcome:
+      "Welcome to the GeeksforGeeks " +
+      "Portal! You can choose any language " +
+      "using the buttons above!",
+    desc: "Energetic frontend developer adept at writing well-designed code and responsive websites with a mobile-first approach.",
+  },
+  es: {
+    welcome:
+      "¡Bienvenido al portal GeeksforGeeks! " +
+      "¡Puedes elegir cualquier idioma usando " +
+      "los botones de arriba!",
+      desc: "Enérgico desarrollador frontend experto en escribir código bien diseñado y sitios web responsivos con un enfoque centrado en los dispositivos móviles."
+  },
+  de: {
+    welcome: "GeeksforGeeks ist eine sehr schone platform",
+    desc: "Energischer Frontend-Entwickler, der sich mit dem Schreiben von gut gestaltetem Code und reaktionsfähigen Websites mit Mobile-First-Ansatz auskennt."
+  },
+};
 
-
-   // Create a function to change
-        // the hash value of the page
-        function changeLanguage(lang) {
-            location.hash = lang;
-            location.reload();
-        }
-
-        // Define the language reload anchors
-        let language = {
-            eng: {
-                welcome: "Welcome to the GeeksforGeeks " +
-                    "Portal! You can choose any language " +
-                    "using the buttons above!"
-            },
-            es: {
-                welcome: "¡Bienvenido al portal GeeksforGeeks! " +
-                    "¡Puedes elegir cualquier idioma usando " +
-                    "los botones de arriba!"
-            },
-            de: {
-                welcome: "GeeksforGeeks ist eine sehr schone platform"
-            }
-        };
-
-        // Check if a hash value exists in the URL
-        if (window.location.hash) {
-
-            // Set the content of the webpage
-            // depending on the hash value
-            if (window.location.hash == "#es") {
-                siteContent.textContent =
-                    language.es.welcome;
-            }
-            else if (window.location.hash == "#de") {
-                siteContent.textContent =
-                    language.de.welcome;
-            }
-        }
+// Check if a hash value exists in the URL
+if (window.location.hash) {
+  // Set the content of the webpage
+  // depending on the hash value
+  if (window.location.hash == "#es") {
+    siteContent.textContent = language.es.welcome;
+    siteContent.textContent = language.es.desc;
+  } else if (window.location.hash == "#en") {
+    siteContent.textContent = language.en.welcome;
+    siteContent.textContent = language.en.desc;
+  }
+}
